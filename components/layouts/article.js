ed12fad19c57038655628008917f7cb03b204e4c
akshay <akshayrajareddy6@gmail.com>
2022-05-20 11:36:43 +0530

    fixed layout title issue

--- components/layouts/article.js
+++ components/layouts/article.js
@@ -9,7 +9,7 @@ const variants = {
 }
 
 const Layout = ({ children, title }) => {
-  const t = `${title} - Takuya Matsuyama`
+  const t = `${title} - Akshay Raja Reddy`
   return (
     <motion.article
       initial="hidden"

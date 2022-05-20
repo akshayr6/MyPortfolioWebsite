bf88498bcbb58aed5f658a1a5ccb39665179e6a3
akshay <akshayrajareddy6@gmail.com>
2022-05-20 11:21:43 +0530

    resolved footer issue

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bed0e14752f43087"; }
+/******/ 		__webpack_require__.h = function() { return "8d83ceae6029aaba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
